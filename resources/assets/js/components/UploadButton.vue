5ba9abe233f439a43b0b51c1da57213d897cf7a5
Uriah Galang <vuetified@gmail.com>
2017-11-29 17:28:06 +0800

    added UploadButton

---
+++ resources/assets/js/components/UploadButton.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-btn flat icon color="cyan" dark class="jbtn-file">
+   <v-icon>fa-upload</v-icon>
+   <input id="selectFile" type="file" v-on:change="fileSelected">
+  </v-btn>
+</template>
+
+<script>
+export default {
+    props: {
+        selectedCallback: Function,
+        title: String
+    },
+    methods: {
+        fileSelected (e) {
+            if (this.selectedCallback) {
+                if (e.target.files[0]) {
+                    this.selectedCallback(e.target.files[0])
+                } else {
+                    this.selectedCallback(null)
+                }
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+  .jbtn-file {
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+    bottom: -15px;
+  }
+
+  .jbtn-file input[type=file] {
+    position: absolute;
+    top: 0;
+    right: 0;
+    width: 100%;
+    height: 100%;
+    font-size: 100px;
+    text-align: right;
+    filter: alpha(opacity=0);
+    opacity: 0;
+    outline: none;
+    cursor: inherit;
+    display: block;
+  }
+</style>
