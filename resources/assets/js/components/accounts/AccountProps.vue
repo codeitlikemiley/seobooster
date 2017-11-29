8254f24b85eb99253f312b45f7074225180e2803
Uriah Galang <vuetified@gmail.com>
2017-11-29 17:28:23 +0800

    added accountes components

---
+++ resources/assets/js/components/accounts/AccountProps.vue
@@ -0,0 +1,179 @@
+<template>
+<v-card light>
+    <v-card-title>
+    <v-spacer></v-spacer>
+    <v-text-field
+    light
+    append-icon="search"
+    label="Search By Username"
+    single-line
+    hide-details
+    v-model="search"
+    ></v-text-field>
+    </v-card-title>
+    <v-data-table
+    :items="items"
+    :search="search"
+    v-model="selected"
+    :headers="headers"
+    :pagination.sync="pagination"
+    item-key="id"
+    select-all
+    light
+    >
+        <template slot="headers" slot-scope="props">
+            <th>
+                <v-checkbox
+                light
+                primary
+                hide-details
+                @click.native="toggleAll"
+                :input-value="props.all"
+                :indeterminate="props.indeterminate"
+                ></v-checkbox>
+            </th>
+            <th v-for="header in props.headers" :key="header.text"
+                :class="['column sortable', pagination.descending ? 'desc' : 'asc', header.value === pagination.sortBy ? 'active' : '']"
+                @click="changeSort(header.value)"
+            >
+                <v-icon>arrow_upward</v-icon>
+                {{ header.text }}
+            </th>
+            <th class="text-xs-center">
+                <span v-if="selected.length < 1">Actions</span>
+                <v-btn v-else flat icon color="red darken-4" @click.native="deleteSelected()">
+                <v-icon>fa-trash</v-icon>
+            </v-btn>
+            </th>
+        </template>
+        <template slot="items" slot-scope="props">
+            <td>
+                <v-checkbox
+                light
+                primary
+                hide-details
+                v-model="props.selected"
+                ></v-checkbox>
+            </td>
+            <td class="text-xs-left">
+                <v-edit-dialog
+              @open="tmp = props.item.username"
+              @save="props.item.username = tmp || props.item.username"
+              large
+              lazy
+            >
+              <div>{{ props.item.username }}</div>
+              <div slot="input" class="mt-3 title">Update Username</div>
+              <v-text-field
+                slot="input"
+                label="Edit Username"
+                v-model="tmp"
+                single-line
+                counter
+                autofocus
+                :rules="[max25chars]"
+              ></v-text-field>
+            </v-edit-dialog>
+            </td>
+            <td class="text-xs-center">
+                <v-chip :class="{ 'green': isActive(props.item),'amber': !isActive(props.item) }" text-color="white">
+                <span>{{ `${props.item.active ? 'Active' : 'Inactive'}` }}</span>
+                </v-chip>
+            </td>
+            <td class="text-xs-left">{{ props.item.activated_at }}</td>
+            <td class="text-xs-left">{{ props.item.expired_at }}</td>
+            <th class="text-xs-center">{{  props.item.post_count }}</th>
+            <td class="text-xs-center">
+                <!-- //! View The User Profile Page -->
+                <v-btn flat icon color="cyan" @click.native="">
+                <v-icon>fa-eye</v-icon>
+                </v-btn>
+                <!-- //! Only Show This When Account is Not Yet Active -->
+                <v-btn flat icon color="green darken-2" @click.native="">
+                <v-icon>fa-sign-in</v-icon>
+                </v-btn>
+                <!-- //! Only Show This When Token is Expired -->
+                <v-btn v-if="tokenExpired(props.item)" flat icon color="indigo darken-2" @click.native="">
+                <v-icon>fa-refresh</v-icon>
+                </v-btn>
+                <!-- //! Edit The User Credentials -->
+                <v-btn flat icon color="accent" @click.native="">
+                    <v-icon>fa-edit</v-icon>
+                </v-btn>
+                <!-- //! Delete Account -->
+                <v-btn flat icon color="error" @click.native="">
+                    <v-icon>fa-remove</v-icon>
+                </v-btn>
+            </td>
+        </template>
+        <template slot="pageText" slot-scope="{ pageStart, pageStop }">
+            From {{ pageStart }} to {{ pageStop }}
+        </template>
+    </v-data-table>
+</v-card>
+</template>
+
+<script>
+export default {
+    props: ['accounts'],
+    data: () => ({
+        selected: [],
+        pagination: {
+            sortBy: 'name'
+        },
+        search: '',
+        /* table */
+        headers: [
+            /* remove sort and value since we cant access dot anotation in item */
+            { text: 'Username', value: 'username', align: 'left', sortable: true },
+            { text: 'Status', value: 'active', align: 'left', sortable: true },
+            { text: 'Token Creation Date', value: 'activated_at', align: 'left', sortable: true },
+            { text: 'Token Expiration Date', value: 'expired_at', align: 'left', sortable: true },
+            { text: 'Total Post', value: 'post_count', center: 'left', sortable: true }
+
+        ],
+        items: [],
+        max25chars: (v) => v.length <= 25 || 'Input too long!',
+        tmp: ''
+    }),
+    mounted () {
+        this.items = this.accounts
+    },
+    methods: {
+        toggleAll () {
+            if (this.selected.length) this.selected = []
+            else this.selected = this.items.slice()
+        },
+        isActive (item) {
+            return !!item.active
+        },
+        changeSort (column) {
+            if (this.pagination.sortBy === column) {
+                this.pagination.descending = !this.pagination.descending
+            } else {
+                this.pagination.sortBy = column
+                this.pagination.descending = false
+            }
+        },
+        deleteAll () {
+            this.items = []
+            self.selected = []
+        },
+        deleteSelected () {
+            let self = this
+            let newItems = _.difference(self.items, self.selected)
+            self.items = newItems
+            self.selected = []
+            //! Send Api Call To Delete The Social Account
+        },
+        tokenExpired (item) {
+            //! Compare Current Date with Date of Expiration
+            return false
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ resources/assets/js/components/accounts/VideoAccounts.vue
@@ -0,0 +1,178 @@
+<template>
+<v-card light>
+    <v-card-title>
+    <v-text-field
+    light
+    append-icon="search"
+    label="Search By Platform"
+    single-line
+    hide-details
+    v-model="search"
+    ></v-text-field>
+    </v-card-title>
+    <v-data-table
+    :headers="headers"
+    :items="items"
+    :search="search"
+    item-key="id"
+    :pagination.sync="pagination"
+    light
+    expand
+    >
+        <template slot="headers" slot-scope="props">
+            <tr>
+            <th v-for="header in props.headers" :key="header.text"
+                :class="['column sortable', pagination.descending ? 'desc' : 'asc', header.value === pagination.sortBy ? 'active' : '']"
+                @click="changeSort(header.value)"
+            >
+                <v-icon>arrow_upward</v-icon>
+                {{ header.text }}
+            </th>
+            <th>
+                Actions
+            </th>
+            </tr>
+        </template>
+        <template slot="items" slot-scope="props">
+                <td class="title text-xs-left primary--text">
+                    <v-icon :color="props.item.iconColor">{{ props.item.icon }}</v-icon>
+                    <span class="caption accent--text">{{ props.item.platform }}</span>
+                </td>
+                <td class="title text-xs-center primary--text">
+                    <span class="title blue-grey--text">{{ props.item.accounts.length }}</span>
+                </td>
+                <td class="title text-xs-center">
+                    <v-btn light  flat icon :class="{'amber--text': props.expanded, 'amber': props.expanded, 'teal': !props.expanded, 'teal--text': !props.expanded }" @click="props.expanded = !props.expanded">
+                    <v-icon v-if="!props.expanded">fa-expand</v-icon>
+                    <v-icon v-if="props.expanded">fa-compress</v-icon>
+                    </v-btn>
+                    <v-btn flat icon color="accent" @click.native="">
+                        <v-icon>fa-plus</v-icon>
+                    </v-btn>
+                </td>
+            </tr>
+        </template>
+        <!--
+        //!Working Only in vuetify v 0.17
+        <template slot="no-data">
+            <v-alert :value="true" color="error" icon="warning">
+            Sorry, nothing to display here :(
+            </v-alert>
+        </template>
+        -->
+        <template slot="expand" slot-scope="props">
+        <account-props :accounts='props.item.accounts'></account-props>
+        </template>
+
+        <template slot="pageText" slot-scope="{ pageStart, pageStop }">
+            From {{ pageStart }} to {{ pageStop }}
+        </template>
+
+    </v-data-table>
+</v-card>
+</template>
+
+<script>
+import AccountProps from './AccountProps'
+//! Fetch Video Accounts From Database
+export default {
+    props: ['tab'],
+    components: {
+        AccountProps
+    },
+    data: () => ({
+        pagination: {
+            sortBy: 'name'
+        },
+        search: '',
+        /* table */
+        headers: [
+            /* remove sort and value since we cant access dot anotation in item */
+            { text: 'Platform', value: 'platform', align: 'left', sortable: true },
+            { text: 'Accounts #', value: 'accounts.length', align: 'left', sortable: false }
+        ],
+        items: [
+            {
+                id: 1,
+                type: 'video-accounts',
+                icon: 'fa-youtube',
+                platform: 'youtube',
+                iconColor: 'red darken-4',
+                accounts: [
+                    {
+                        id: 1,
+                        username: 'uriahg@facebook.com',
+                        active: true,
+                        activated_at: '16 Nov 2017 @ 10:15:01',
+                        expired_at: '18 Nove 2017 @ 10:16:01',
+                        post_count: 10
+                    },
+                    {
+                        id: 2,
+                        username: 'uriahg1@facebook.com',
+                        active: true,
+                        activated_at: '16 Nov 2017 @ 10:15:01',
+                        expired_at: '18 Nove 2017 @ 10:16:01',
+                        post_count: 10
+                    },
+                    {
+                        id: 3,
+                        username: 'uriahg2@facebook.com',
+                        active: true,
+                        activated_at: '16 Nov 2017 @ 10:15:01',
+                        expired_at: '18 Nove 2017 @ 10:16:01',
+                        post_count: 10
+                    },
+                    {
+                        id: 4,
+                        username: 'uriahg3@facebook.com',
+                        active: true,
+                        activated_at: '16 Nov 2017 @ 10:15:01',
+                        expired_at: '18 Nove 2017 @ 10:16:01',
+                        post_count: 10
+                    },
+                    {
+                        id: 5,
+                        username: 'uriah4g@facebook.com',
+                        active: true,
+                        activated_at: '16 Nov 2017 @ 10:15:01',
+                        expired_at: '18 Nove 2017 @ 10:16:01',
+                        post_count: 10
+                    }
+                ]
+            },
+            {
+                id: 2,
+                type: 'video-accounts',
+                icon: 'fa-vimeo',
+                platform: 'vimep',
+                iconColor: 'cyan',
+                accounts: [
+                    {
+                        id: 2,
+                        username: 'uriahg17',
+                        active: true,
+                        activated_at: '16 Nov 2017 @ 10:15:01',
+                        expired_at: '18 Nove 2017 @ 10:16:01',
+                        post_count: 1
+                    }
+                ]
+            }
+        ]
+    }),
+    methods: {
+        changeSort (column) {
+            if (this.pagination.sortBy === column) {
+                this.pagination.descending = !this.pagination.descending
+            } else {
+                this.pagination.sortBy = column
+                this.pagination.descending = false
+            }
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
