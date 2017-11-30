0cc1697596ee94870ec3e4eb23e10830c5edc108
Uriah Galang <vuetified@gmail.com>
2017-11-30 17:25:03 +0800

    Added Failre, PUblished , and Scheduled Route

--- resources/assets/js/routes/dashboardRoutes.js
+++ resources/assets/js/routes/dashboardRoutes.js
@@ -1,6 +1,9 @@
 const Accounts = () => import('../pages/Accounts.vue')
 const Reports = () => import('../pages/Reports.vue')
 const Posts = () => import('../pages/Posts.vue')
+const Failure = () => import('../pages/Failure.vue')
+const Published = () => import('../pages/Published.vue')
+const Scheduled = () => import('../pages/Scheduled.vue')
 export default [
     /* Start Dashboard Routes */
     {
@@ -27,6 +30,36 @@ export default [
         path: '/posts',
         component: Posts,
         name: 'posts',
+        meta: {
+            requiresAuth: true,
+            permission: 'guest',
+            fail: '/error'
+        }
+    },
+    {
+        path: '/failure',
+        component: Failure,
+        name: 'failure',
+        meta: {
+            requiresAuth: true,
+            permission: 'guest',
+            fail: '/error'
+        }
+    },
+    {
+        path: '/published',
+        component: Published,
+        name: 'published',
+        meta: {
+            requiresAuth: true,
+            permission: 'guest',
+            fail: '/error'
+        }
+    },
+    {
+        path: '/scheduled',
+        component: Scheduled,
+        name: 'scheduled',
         meta: {
             requiresAuth: true,
             permission: 'guest',
