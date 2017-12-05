daa2610e5196bdb593b97febc67b606d59149e4d
Uriah Galang <vuetified@gmail.com>
2017-12-05 16:09:46 +0800

    working twitter model

--- app/Twitter.php
+++ app/Twitter.php
@@ -9,18 +9,23 @@ class Twitter extends Model
     protected $table = "twitter_accounts";
 
     protected $fillable = [
-        'username', 'access_token', 'refresh_token', 'expires_at', 'scope'
+        'username', 'access_token', 'access_token_secret', 'expires_at', 'scope', 'revoked'
     ];
 
     protected $dates = ['created_at', 'updated_at', 'expires_at'];
 
     protected $casts = [
         'scope' => 'array', 
+        'revoked' => 'boolean'
     ];
-
 
     public function accounts()
     {
-        return $this->morphMany(Provider::class, 'accounts');
+        return $this->morphToMany(Account::class, 'accountable');
+    }
+
+    public function owner()
+    {
+        return $this->belongsTo(User::class);
     }
 }
