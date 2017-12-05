0fb73cb49d695e5d0451379757a6e87fe042e56a
Uriah Galang <vuetified@gmail.com>
2017-12-05 16:19:35 +0800

    added Provider model

---
+++ app/Provider.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
+use Cviebrock\EloquentSluggable\SluggableScopeHelpers;
+
+class Provider extends Model
+{
+    use Sluggable,SluggableScopeHelpers;
+
+    protected $fillable = [
+       'type', 'name', 'slug', 'client_id', 'client_secret', 'redirect_url', 'scope', 'config'
+    ];
+
+    protected $dates = ['created_at', 'updated_at'];
+
+    protected $casts = [
+        'scope' => 'array',
+        'config' => 'array',
+    ];
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+    public function sluggable()
+    {
+        return [
+            'slug' => [
+                'source' => 'name'
+            ]
+        ];
+    }
+
+}
