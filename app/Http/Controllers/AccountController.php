10803454b52f7976b0b1106c9a13ff59d4ff29c0
Uriah Galang <vuetified@gmail.com>
2017-12-05 16:26:06 +0800

    remove not needed method

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Account;
+use App\Provider;
 use Illuminate\Http\Request;
+use App\Exceptions\UserTokenNotFound;
 
 class AccountController extends Controller
 {
@@ -12,9 +14,13 @@ class AccountController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $user = $request->user();
+        if(!$user){
+            throw new UserTokenNotFound;
+        }
+        return Account::where('user_id' ,$user->id);
     }
 
     /**
@@ -33,53 +39,19 @@ class AccountController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(Request $request,$type,$provider)
     {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Account  $account
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Account $account)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Account  $account
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Account $account)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Account  $account
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Account $account)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Account  $account
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Account $account)
-    {
-        //
+        $provider = Provider::findBySlugOrFail($provider);
+        $account = new Account();
+        $account->type = $type;
+        $account->user_id = $request->user()->id;
+        $provider->accounts()->save($account);
+        $account->save();
+        $newAccount = new $provider->model;
+        /* Add dynamic validation */
+        $newAccount->fill($request->all());
+        $newAccount->save();
+        $newAccount->accounts()->save($provider);
+        return response()->json(['mesage' => 'New Account Added'],200);
     }
 }
