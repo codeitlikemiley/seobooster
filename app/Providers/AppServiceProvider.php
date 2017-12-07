110943fbea0c145fe2ba39ad557bf213fa0553ba
Uriah Galang <vuetified@gmail.com>
2017-12-07 17:15:51 +0800

    added hook to save post updated at column

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,11 +2,16 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
-use App\Observers\UserObserver;
-use App\Observers\LinkObserver;
 use App\User;
 use App\Link;
+use App\Observers\UserObserver;
+use App\Observers\LinkObserver;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Queue;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Queue\Events\JobProcessed;
+use Illuminate\Queue\Events\JobProcessing;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +24,30 @@ class AppServiceProvider extends ServiceProvider
     {
         User::observe(UserObserver::class);
         Link::observe(LinkObserver::class);
+
+        //! Update Posted AT
+        Queue::after(function (JobProcessed $event) {
+            $string = $event->job->payload()['tags'][0];
+            $modelEntities = explode(':', $string, 2); //
+            //? Separate Model and ID in array ["App\TwitterPost","1",]
+            $model =  $modelEntities[0];
+            $modelId = $modelEntities[1];
+            //! Create New Instance Of Model
+            $model = new $model;
+            //! Find Model by ID
+            $post = $model->find($modelId);
+            //! Update Updated @
+            $post->posted_at = true;
+            $post->save();
+            Log::info('Job Object:',$post->toArray());
+        });
+        //!  rollback any transactions that were left open by a previously failed job
+        Queue::looping(function () {
+            while (DB::transactionLevel() > 0) {
+                DB::rollBack();
+            }
+        });
+        
     }
 
     /**
@@ -28,6 +57,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        
     }
 }
