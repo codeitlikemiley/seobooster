796e28c2c2aacfd7d1ab2c9deedd7426f5f03461
Uriah Galang <vuetified@gmail.com>
2017-12-15 16:15:21 +0800

    test 1

--- app/Http/Controllers/AccountProviderController.php
+++ app/Http/Controllers/AccountProviderController.php
@@ -34,7 +34,7 @@ class AccountProviderController extends Controller
         \Auth::loginUsingId($user->id);
         $account = $user->accounts->where('name', $provider)->first();
         $config = new \SocialiteProviders\Manager\Config($account->client_id, $account->client_secret, $account->redirect_url);
-        $user = \Socialite::with($provider)->setConfig($config)->user();
+        $user = \Socialite::driver($provider)->user();
         // $user = \Socialite::driver($provider)->getAccessTokenResponse($request->code);
         // //! Process provider for the user...
 
