1ce78f3dd31477863827a890e2195165f989f3c3
Uriah Galang <vuetified@gmail.com>
2017-12-15 16:59:08 +0800

    return stateless

--- app/Http/Controllers/AccountProviderController.php
+++ app/Http/Controllers/AccountProviderController.php
@@ -46,7 +46,7 @@ class AccountProviderController extends Controller
         // "screen_name": "uriahg17",
         // "x_auth_expires": "0"
         // Save this to the Twitter Database
-        return  response()->json($request->all());
+        return \Socialite::driver($provider)->stateless()->user();
         // POST API by twitter
         // https://developer.twitter.com/en/docs/tweets/post-and-engage/api-reference/post-statuses-update
     }
