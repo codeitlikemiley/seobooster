e3f974ad3aebd0d502e9418414464c1a060d5902
Uriah <codeitlikemiley@gmail.com>
2017-12-19 11:24:13 +0800

    added user method for twitter provider

--- SocialiteProviders/Twitter/src/Provider.php
+++ SocialiteProviders/Twitter/src/Provider.php
@@ -28,12 +28,19 @@ class Provider extends AbstractProvider
 
     public function user()
     {
-        $response = $this->getAccessTokenResponse($this->getCode());
-
-        $user = $this->mapUserToObject($this->getUserByToken(
-            $token = Arr::get($response, 'access_token')
-        ));
-
-        return $user;
+        if (! $this->hasNecessaryVerifier()) {
+            throw new InvalidArgumentException('Invalid request. Missing OAuth verifier.');
+        }
+        $user = $this->server->getUserDetails($token = $this->getToken());
+        $extraDetails = [
+            'location' => $user->location,
+            'description' => $user->description,
+        ];
+        $instance = (new User)->setRaw(array_merge($user->extra, $user->urls, $extraDetails));
+        return $instance->map([
+            'id' => $user->uid, 'nickname' => $user->nickname,
+            'name' => $user->name, 'email' => $user->email, 'avatar' => $user->imageUrl,
+            'avatar_original' => str_replace('_normal', '', $user->imageUrl),
+        ]);
     }
 }
