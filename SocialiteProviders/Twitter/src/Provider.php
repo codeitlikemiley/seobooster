7175eac8328d824baeaf297912e2b6e1b8d42c71
Uriah <codeitlikemiley@gmail.com>
2017-12-19 11:28:48 +0800

    added useMapObject

--- SocialiteProviders/Twitter/src/Provider.php
+++ SocialiteProviders/Twitter/src/Provider.php
@@ -12,9 +12,6 @@ class Provider extends AbstractProvider
      */
     const IDENTIFIER = 'TWITTER';
 
-    /**
-     * {@inheritDoc}
-     */
     protected function mapUserToObject(array $user)
     {
         return (new User())->setRaw($user['extra'])->map([
@@ -23,24 +20,6 @@ class Provider extends AbstractProvider
              'name' => $user['name'],
              'email' => $user['email'],
              'avatar' => $user['avatar'],
-        ]);
-    }
-
-    public function user()
-    {
-        if (! $this->hasNecessaryVerifier()) {
-            throw new InvalidArgumentException('Invalid request. Missing OAuth verifier.');
-        }
-        $user = $this->server->getUserDetails($token = $this->getToken());
-        $extraDetails = [
-            'location' => $user->location,
-            'description' => $user->description,
-        ];
-        $instance = (new User)->setRaw(array_merge($user->extra, $user->urls, $extraDetails));
-        return $instance->map([
-            'id' => $user->uid, 'nickname' => $user->nickname,
-            'name' => $user->name, 'email' => $user->email, 'avatar' => $user->imageUrl,
-            'avatar_original' => str_replace('_normal', '', $user->imageUrl),
         ]);
     }
 }
