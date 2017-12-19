31f8c1fdf11706f5ad99b2b25148630b41f792cd
Uriah <codeitlikemiley@gmail.com>
2017-12-19 11:52:49 +0800

    added video service provider

---
+++ SocialiteProviders/Vimeo/src/Provider.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace SocialiteProviders\Vimeo;
+
+use SocialiteProviders\Manager\OAuth2\User;
+use Laravel\Socialite\Two\ProviderInterface;
+use SocialiteProviders\Manager\OAuth2\AbstractProvider;
+
+class Provider extends AbstractProvider implements ProviderInterface
+{
+    /**
+     * Unique Provider Identifier.
+     */
+    const IDENTIFIER = 'VIMEO';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getAuthUrl($state)
+    {
+        return $this->buildAuthUrlFromBase(
+            'https://api.vimeo.com/oauth/authorize', $state
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenUrl()
+    {
+        return 'https://api.vimeo.com/oauth/access_token';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getUserByToken($token)
+    {
+        $response = $this->getHttpClient()->get(
+            'https://api.vimeo.com/me', [
+            'headers' => [
+                'Authorization' => 'bearer '.$token,
+            ],
+        ]);
+
+        return json_decode($response->getBody()->getContents(), true);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function mapUserToObject(array $user)
+    {
+        return (new User())->setRaw($user)->map([
+            'id' => str_replace('/users/', null, $user['uri']),
+            'nickname' => null, 'name' => $user['name'], 'email' => null,
+            'avatar' => null,
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenFields($code)
+    {
+        return array_merge(parent::getTokenFields($code), [
+            'grant_type' => 'authorization_code',
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function user()
+    {
+        if ($this->hasInvalidState()) {
+            throw new InvalidStateException;
+        }
+
+        $response = $this->getAccessTokenResponse($this->getCode());
+
+        $user = $this->mapUserToObject($this->getUserByToken(
+            $token = Arr::get($response, 'access_token')
+        ));
+
+        return $user;
+    }
+}
---
+++ SocialiteProviders/Vimeo/src/VimeoExtendSocialite.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace SocialiteProviders\Vimeo;
+
+use SocialiteProviders\Manager\SocialiteWasCalled;
+
+class VimeoExtendSocialite
+{
+    /**
+     * Execute the provider.
+     */
+    public function handle(SocialiteWasCalled $socialiteWasCalled)
+    {
+        $socialiteWasCalled->extendSocialite('vimeo', __NAMESPACE__.'\Provider');
+    }
+}
