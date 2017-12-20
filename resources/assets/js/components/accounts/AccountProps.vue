6b7b3ed1966e406c8f0912bd61e449137dd1834b
Uriah Galang <vuetified@gmail.com>
2017-12-20 09:30:01 +0800

    fix html for AccountProps.vue

--- resources/assets/js/components/accounts/AccountProps.vue
+++ resources/assets/js/components/accounts/AccountProps.vue
@@ -1,121 +1,176 @@
 <template>
-<v-card light>
+  <v-card light>
     <v-card-title>
-    <v-spacer></v-spacer>
-    <v-text-field
-    light
-    append-icon="search"
-    label="Search By Username"
-    single-line
-    hide-details
-    v-model="search"
-    ></v-text-field>
+      <v-spacer/>
+      <v-text-field
+        light
+        append-icon="search"
+        label="Search By Username"
+        single-line
+        hide-details
+        v-model="search"
+      />
     </v-card-title>
     <v-data-table
-    :items="items"
-    :search="search"
-    v-model="selected"
-    :headers="headers"
-    :pagination.sync="pagination"
-    item-key="id"
-    select-all
-    light
+      :items="items"
+      :search="search"
+      v-model="selected"
+      :headers="headers"
+      :pagination.sync="pagination"
+      item-key="id"
+      select-all
+      light
     >
-        <template slot="headers" slot-scope="props">
-            <th>
-                <v-checkbox
-                light
-                primary
-                hide-details
-                @click.native="toggleAll"
-                :input-value="props.all"
-                :indeterminate="props.indeterminate"
-                ></v-checkbox>
-            </th>
-            <th v-for="header in props.headers" :key="header.text"
-                :class="['column sortable', pagination.descending ? 'desc' : 'asc', header.value === pagination.sortBy ? 'active' : '']"
-                @click="changeSort(header.value)"
+      <template
+        slot="headers" 
+        slot-scope="props">
+        <th>
+          <v-checkbox
+            light
+            primary
+            hide-details
+            @click.native="toggleAll"
+            :input-value="props.all"
+            :indeterminate="props.indeterminate"
+          />
+        </th>
+        <th 
+          v-for="header in props.headers" 
+          :key="header.text"
+          :class="['column sortable', pagination.descending ? 'desc' : 'asc', header.value === pagination.sortBy ? 'active' : '']"
+          @click="changeSort(header.value)"
+        >
+          <v-icon>arrow_upward</v-icon>
+          {{ header.text }}
+        </th>
+        <th class="text-xs-center">
+          <span v-if="selected.length < 1">Actions</span>
+          <v-btn 
+            v-else 
+            flat 
+            icon 
+            color="red darken-4" 
+            @click.native="deleteSelected()"
+          >
+            <v-icon>fa-trash</v-icon>
+          </v-btn>
+        </th>
+      </template>
+      <template 
+        slot="items" 
+        slot-scope="props">
+        <td>
+          <v-checkbox
+            light
+            primary
+            hide-details
+            v-model="props.selected"
+          />
+        </td>
+        <td class="text-xs-left">
+          <v-edit-dialog
+            @open="tmp = props.item.username"
+            @save="props.item.username = tmp || props.item.username"
+            large
+            lazy
+          >
+            <div>{{ props.item.username }}</div>
+            <div 
+              slot="input" 
+              class="mt-3 title"
             >
-                <v-icon>arrow_upward</v-icon>
-                {{ header.text }}
-            </th>
-            <th class="text-xs-center">
-                <span v-if="selected.length < 1">Actions</span>
-                <v-btn v-else flat icon color="red darken-4" @click.native="deleteSelected()">
-                <v-icon>fa-trash</v-icon>
-            </v-btn>
-            </th>
-        </template>
-        <template slot="items" slot-scope="props">
-            <td>
-                <v-checkbox
-                light
-                primary
-                hide-details
-                v-model="props.selected"
-                ></v-checkbox>
-            </td>
-            <td class="text-xs-left">
-                <v-edit-dialog
-              @open="tmp = props.item.username"
-              @save="props.item.username = tmp || props.item.username"
-              large
-              lazy
-            >
-              <div>{{ props.item.username }}</div>
-              <div slot="input" class="mt-3 title">Update Username</div>
-              <v-text-field
-                slot="input"
-                label="Edit Username"
-                v-model="tmp"
-                single-line
-                counter
-                autofocus
-                :rules="[max25chars]"
-              ></v-text-field>
-            </v-edit-dialog>
-            </td>
-            <td class="text-xs-center">
-                <v-chip :class="{ 'green': isActive(props.item),'amber': !isActive(props.item) }" text-color="white">
-                <span>{{ `${props.item.active ? 'Active' : 'Inactive'}` }}</span>
-                </v-chip>
-            </td>
-            <td class="text-xs-left">{{ props.item.activated_at }}</td>
-            <td class="text-xs-left">{{ props.item.expired_at }}</td>
-            <th class="text-xs-center">{{  props.item.post_count }}</th>
-            <td class="text-xs-center">
-                <!-- //! View The User Profile Page -->
-                <v-btn flat icon color="cyan" @click.native="">
-                <v-icon>fa-eye</v-icon>
-                </v-btn>
-                <!-- //! Only Show This When Account is Not Yet Active -->
-                <v-btn flat icon color="green darken-2" :href="props.item.link" target="_blank">
-                <v-icon>fa-sign-in</v-icon>
-                </v-btn>
-                <!-- //! Only Show This When Token is Expired -->
-                <v-btn v-if="tokenExpired(props.item)" flat icon color="indigo darken-2" @click.native="">
-                <v-icon>fa-refresh</v-icon>
-                </v-btn>
-                <!-- //! Edit The User Credentials -->
-                <v-btn flat icon color="accent" @click.native="">
-                    <v-icon>fa-edit</v-icon>
-                </v-btn>
-                <!-- //! Delete Account -->
-                <v-btn flat icon color="error" @click.native="">
-                    <v-icon>fa-remove</v-icon>
-                </v-btn>
-            </td>
-        </template>
-        <template slot="pageText" slot-scope="{ pageStart, pageStop }">
-            From {{ pageStart }} to {{ pageStop }}
-        </template>
+              Update Username
+            </div>
+            <v-text-field
+              slot="input"
+              label="Edit Username"
+              v-model="tmp"
+              single-line
+              counter
+              autofocus
+              :rules="[max25chars]"
+            />
+          </v-edit-dialog>
+        </td>
+        <td class="text-xs-center">
+          <v-chip 
+            :class="{ 'green': isActive(props.item),'amber': !isActive(props.item) }" 
+            text-color="white"
+          >
+            <span>{{ `${props.item.active ? 'Active' : 'Inactive'}` }}</span>
+          </v-chip>
+        </td>
+        <td class="text-xs-left">{{ props.item.activated_at }}</td>
+        <td class="text-xs-left">{{ props.item.expired_at }}</td>
+        <th class="text-xs-center">{{ props.item.post_count }}</th>
+        <td class="text-xs-center">
+          <!-- //! View The User Profile Page -->
+          <v-btn 
+            flat 
+            icon 
+            color="cyan"
+            @click.native=""
+          >
+            <v-icon>fa-eye</v-icon>
+          </v-btn>
+          <!-- //! Only Show This When Account is Not Yet Active -->
+          <v-btn 
+            flat 
+            icon 
+            color="green darken-2" 
+            :href="props.item.link" 
+            target="_blank"
+          >
+            <v-icon>fa-sign-in</v-icon>
+          </v-btn>
+          <!-- //! Only Show This When Token is Expired -->
+          <v-btn 
+            v-if="tokenExpired(props.item)" 
+            flat 
+            icon 
+            color="indigo darken-2" 
+            @click.native=""
+          >
+            <v-icon>fa-refresh</v-icon>
+          </v-btn>
+          <!-- //! Edit The User Credentials -->
+          <v-btn 
+            flat 
+            icon 
+            color="accent" 
+            @click.native=""
+          >
+            <v-icon>fa-edit</v-icon>
+          </v-btn>
+          <!-- //! Delete Account -->
+          <v-btn 
+            flat 
+            icon 
+            color="error" 
+            @click.native=""
+          >
+            <v-icon>fa-remove</v-icon>
+          </v-btn>
+        </td>
+      </template>
+      <template 
+        slot="pageText" 
+        slot-scope="{ pageStart, pageStop }"
+      >
+        From {{ pageStart }} to {{ pageStop }}
+      </template>
     </v-data-table>
-</v-card>
+  </v-card>
 </template>
 
 <script>
 export default {
-    props: ['accounts'],
+    props:{
+        accounts: {
+            type: Array,
+            required: true
+        }
+    },
     data: () => ({
         selected: [],
         pagination: {
