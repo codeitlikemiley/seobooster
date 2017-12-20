705ba185ee15357e6f909e632e6433ab387e3f20
Uriah Galang <vuetified@gmail.com>
2017-12-20 09:40:34 +0800

    fix provider callback html

--- resources/assets/js/pages/ProviderCallback.vue
+++ resources/assets/js/pages/ProviderCallback.vue
@@ -1,55 +1,75 @@
-    <template>
-    <main-layout :style="{ paddingTop: `100px`, backgroundColor: `white` }">
-        <v-container  fluid>
-            <v-layout row wrap>
-                <v-breadcrumbs>
-                    <v-icon slot="divider" color="teal">forward</v-icon>
-                    <v-breadcrumbs-item
-                    active-class="primary--text"
-                    :disabled="false"
-                    to="/dashboard"
-                    >
-                        Dashboard
-                    </v-breadcrumbs-item>
-                    <v-breadcrumbs-item
-                    :disabled="true"
-                    >
-                        <span class="blue-grey--text">Accounts</span>
-                    </v-breadcrumbs-item>
-                </v-breadcrumbs>
-            </v-layout>
-            <v-layout row wrap>
-            <v-tabs fixed icons centered>
-                    <v-tabs-bar>
-                    <v-tabs-slider color="primary"></v-tabs-slider>
-                    <v-tabs-item
-                    v-for="(tab,key) in tabs"
-                    :key="key"
-                    :href="`#${tab.name}`"
-                    ripple
-                    class="primary--text"
-                    >
-                    <v-icon :color="tab.iconColor">{{tab.icon}}</v-icon>
-                    {{ tab.name }}
-                    </v-tabs-item>
-                    </v-tabs-bar>
-                <v-tabs-items>
-                    <v-tabs-content
-                    v-for="(tab, key) in tabs"
-                    :key="key"
-                    :id="tab.name"
-                    >
-                    <v-card flat :light="true">
-                        <component :is="tab.component" :tab="tab">
-                        </component>
-                    </v-card>
-                    </v-tabs-content>
-                </v-tabs-items>
-            </v-tabs>
-            </v-layout>
-        </v-container>
-    </main-layout>
-    </template>
+<template>
+  <main-layout :style="{ paddingTop: `100px`, backgroundColor: `white` }">
+    <v-container fluid>
+      <v-layout 
+        row 
+        wrap
+      >
+        <v-breadcrumbs>
+          <v-icon 
+            slot="divider" 
+            color="teal"
+          >
+            forward
+          </v-icon>
+          <v-breadcrumbs-item
+            active-class="primary--text"
+            :disabled="false"
+            to="/dashboard"
+          >
+            Dashboard
+          </v-breadcrumbs-item>
+          <v-breadcrumbs-item
+            :disabled="true"
+          >
+            <span class="blue-grey--text">Accounts</span>
+          </v-breadcrumbs-item>
+        </v-breadcrumbs>
+      </v-layout>
+      <v-layout 
+        row 
+        wrap
+      >
+        <v-tabs 
+          fixed 
+          icons 
+          centered
+        >
+          <v-tabs-bar>
+            <v-tabs-slider color="primary"/>
+            <v-tabs-item
+              v-for="(tab,key) in tabs"
+              :key="key"
+              :href="`#${tab.name}`"
+              ripple
+              class="primary--text"
+            >
+              <v-icon :color="tab.iconColor">{{ tab.icon }}</v-icon>
+              {{ tab.name }}
+            </v-tabs-item>
+          </v-tabs-bar>
+          <v-tabs-items>
+            <v-tabs-content
+              v-for="(tab, key) in tabs"
+              :key="key"
+              :id="tab.name"
+            >
+              <v-card 
+                flat 
+                :light="true"
+              >
+                <component 
+                  :is="tab.component" 
+                  :tab="tab"
+                />
+              </v-card>
+            </v-tabs-content>
+          </v-tabs-items>
+        </v-tabs>
+      </v-layout>
+    </v-container>
+  </main-layout>
+</template>
 
 <script>
 
@@ -62,18 +82,18 @@ import { createNamespacedHelpers } from 'vuex'
 const { mapGetters } = createNamespacedHelpers('auth')
 
 export default {
-    props: ['route'],
-    mixins: [Theme],
     components: {
         MainLayout,
         BlogAccounts,
         SocialAccounts,
         VideoAccounts
     },
-    computed: {
-       ...mapGetters({
-            getMe: 'getMe'
-        }) 
+    mixins: [Theme],
+    props:{
+        route: {
+            type: Array,
+            required: true
+        }
     },
     data: () => ({
         accounts: {
@@ -92,6 +112,11 @@ export default {
             name: 'blog accounts'
         }
     }),
+    computed: {
+        ...mapGetters({
+            getMe: 'getMe'
+        }) 
+    },
     created () {
         this.getAccountsByType('blog')
         this.getAccountsByType('social')
@@ -126,16 +151,16 @@ export default {
             let self = this
             self.usersForm.id = self.getMe.id
             let data = self.$route.query
-             for (let k in data) {
+            for (let k in data) {
                 if (data.hasOwnProperty(k)) {
-                self.usersForm[k] = data[k];
+                    self.usersForm[k] = data[k];
                 }
             }
             window.axios.post(`/providers/${self.$route.params.provider}/callback`,self.usersForm)
-            .then((response) => {
-                console.log(response)
+                .then((response) => {
+                    console.log(response)
                 //! Activate the New User
-            })
+                })
         }
     }
 }
